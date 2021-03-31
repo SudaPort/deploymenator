9e94a887f2f98173b20f125f239743e18ea290a3
SudaPort <81242282+SudaPort@users.noreply.github.com>
2021-03-31 11:30:01 +0300

    Update logo.sh

--- logo.sh
+++ logo.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 tput setaf 4; echo "   _____             _         _____              _             ______   _____            _       _       _____    "
-sleep 1
+sleep 0.5
 tput setaf 4; echo "  / ____|           | |       |  __ \            | |           |  ____| / ____|          | |     | |     / ____|   "
-sleep 1
+sleep 0.5
 tput setaf 4; echo " | (___   _   _   __| |  __ _ | |__) |___   _ __ | |_          | |__   | (___            | |     | |    | |        "
-sleep 1
+sleep 0.5
 tput setaf 4; echo "  \___ \ | | | | / _  | / _  ||  ___// _ \ |  __|| __|         |  __|   \___ \           | |     | |    | |        "
-sleep 1
+sleep 0.5
 tput setaf 4; echo "  ____) || |_| || (_| || (_| || |   | (_) || |   | |_   _      | |____  ____) |   _      | |____ | |____| |____  _ "
-sleep 1
+sleep 0.5
 tput setaf 4; echo " |_____/  \__,_| \__,_| \__,_||_|    \___/ |_|    \__| (_)     |______||_____/   (_)     |______||______|\_____|(_)"
-sleep 1
+sleep 0.5
+tput setaf 4; echo "                                                                                                                   "
+sleep 0.5
 tput setaf 4; echo "                                                                                                                   "
 sleep 1
-tput setaf 4; echo "                                                                                                                   "
-sleep 2
 tput setaf 5; echo "*************************************************************************************************************" 
-sleep 1
+sleep 0.5
 tput setaf 1; echo "Copyright (c) SudaPort Electronic Services Co. Ltd. *  All rights reserved."
 tput setaf 1; echo "To Get your Licensed contact us.  https://sudaport.com for license and sales information."
-sleep 1
+sleep 0.5
 tput setaf 5; echo "*************************************************************************************************************"
 sleep 3
 tput setaf 7;
