b956487916707256ad56ec4e880846647e392c85
SudaPort <81242282+SudaPort@users.noreply.github.com>
2021-04-14 17:46:05 +0300

    Update install-new.sh

--- install-new.sh
+++ install-new.sh
@@ -95,20 +95,20 @@ read -ra response -p "Press Enter to start the system deployment process… "
 
 # ===================================================================================================
 
-# Installing and starting docker ====================================================================
-echo "Removing old docker ============================================================================"
-apt update
-apt-get purge docker-ce docker-ce-cli containerd.io
-rm -rf /var/lib/docker
-rm -rf /var/lib/containerd
-echo "Installing new docker ============================================================================"
-apt -y install dirmngr --install-recommends
-apt -y install git curl make apt-transport-https ca-certificates gnupg lsb-release
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
-usermod -aG docker "$SUDO_USER"
-echo "Starting docker ============================================================================"
-service docker start
+# # Installing and starting docker ====================================================================
+# echo "Removing old docker ============================================================================"
+# apt update
+# apt-get purge docker-ce docker-ce-cli containerd.io
+# rm -rf /var/lib/docker
+# rm -rf /var/lib/containerd
+# echo "Installing new docker ============================================================================"
+# apt -y install dirmngr --install-recommends
+# apt -y install git curl make apt-transport-https ca-certificates gnupg lsb-release
+# curl -fsSL https://get.docker.com -o get-docker.sh
+# sh get-docker.sh
+# usermod -aG docker "$SUDO_USER"
+# echo "Starting docker ============================================================================"
+# service docker start
 
 # Install docker-compose 
 curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
