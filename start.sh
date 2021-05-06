dc69cf5018921f9caf91ce443fe5cfd50f2bc2e7
SudaPort <sudaport1@gmail.com>
2021-05-06 21:30:43 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -192,12 +192,14 @@ sleep 3
 echo "*********************************${GREEN}Generating seeds${NC} *********************************************************"
 GENSEED="$(docker run --rm crypto/core src/stellar-core gen-seed)"
 MASTER_SEED=${GENSEED:13:56}
-MASTER_PUBLIC_KEY=${GENSEED:79:56}
+MASTER_PUBLIC_KEY=${GENSEED:78:56}
 
 GENSEED="$(docker run --rm crypto/core src/stellar-core gen-seed)"
 COMISSION_SEED=${GENSEED:13:56}
-COMISSION_PUBLIC_KEY=${GENSEED:79:56}
-
+COMISSION_PUBLIC_KEY=${GENSEED:78:56}
+echo $GENSEED
+echo $COMISSION_SEED
+echo $COMISSION_PUBLIC_KEY
 # rm -f ${DEPLOYMENATOR_DIR}/${SEEDS_FILE}
 echo "MASTER_SEED=${MASTER_SEED}" >> ${DEPLOYMENATOR_DIR}/${SEEDS_FILE}
 echo "MASTER_PUBLIC_KEY=${MASTER_PUBLIC_KEY}" >> ${DEPLOYMENATOR_DIR}/${SEEDS_FILE}
@@ -220,7 +222,7 @@ sleep 3
 echo "*****************************************************${GREEN}Creating validator${NC} ****************************************"
 GENSEED="$(docker run --rm crypto/core src/stellar-core gen-seed)"
 NODE_SEED=${GENSEED:13:56}
-NODE_PUBLIC_KEY=${GENSEED:79:56}
+NODE_PUBLIC_KEY=${GENSEED:78:56}
 
 IS_VALIDATOR='true'
 RIAK_PROTOCOL_HOST_PORT="http://${HOST_IP}:${RIAK_PORT}" 
